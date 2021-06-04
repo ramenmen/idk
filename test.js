a625114c3543120c3e445c82a7690ab71fab1637
CY <66139098+ramenmen@users.noreply.github.com>
2021-06-04 22:26:58 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
-
+//the first line of code states that the script will run only after the page has loaded 
+//and the script is ready to be executed.
+$(document).ready(function ()  
+{
+//select the close by class and apply a click even listener
+  $(".close").on("click", function () 
+	{
+		//select the modal element by id , and apply display none when close is clicked
+		//this will close the modal on click
+		$("#modal").css("display", "none")
+	});
+  
+});
